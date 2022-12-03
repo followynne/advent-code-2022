425dda7e5570f7eb34eb641101bbbde1996d208f
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2022-12-03 20:27:21 +0000

    feat(3): 2nd algo

--- 3/algo.ts
+++ 3/algo.ts
@@ -4,24 +4,46 @@ const firstQuestion = () => {
   let result = 0;
   inputData.forEach((input) => {
     const [firsthalf, secondHalf] = [
-      input.slice(0, (input.length) / 2),
-      input.slice((input.length) / 2, input.length),
+      input.slice(0, input.length / 2),
+      input.slice(input.length / 2, input.length),
     ];
     const findItem =
       secondHalf.split('').find((p) => firsthalf.includes(p)) ||
       firsthalf.split('').find((p) => secondHalf.includes(p)) ||
       secondHalf.at(0);
 
-    const multiply = findItem!.charCodeAt(0) < 97;
-
-    result += findItem!.toLowerCase().charCodeAt(0) - 96 + (multiply ? 26 : 0);
+    result += calcCharWeight(findItem!);
   });
   return result;
 };
 
+const calcCharWeight = (char?: string) => {
+  if (!char) {
+    console.log(char);
+    throw new Error('Fuck!');
+  }
+  const multiply = char.charCodeAt(0) < 97;
+  return char.toLowerCase().charCodeAt(0) - 96 + (multiply ? 26 : 0);
+};
+
 /* -------------------------------------- */
 
-const secondQuestion = () => {};
+const secondQuestion = () => {
+  const data = [...inputData];
+  let result = 0;
+  do {
+    const firstItems = data.splice(0, 3);
+    const uniques = firstItems.join('').split('').filter(onlyUnique);
+    result += calcCharWeight(
+      uniques.find((p) => firstItems.every((s) => s.includes(p))),
+    );
+  } while (data.length);
+  return result;
+};
+
+function onlyUnique(value: string, index: number, self: string[]) {
+  return self.indexOf(value) === index;
+}
 
 export const Solution3 = {
   1: firstQuestion,
