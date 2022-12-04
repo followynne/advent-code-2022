c90f78a73609fb8ca15b73d0baef2bcd5ba4db7a
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2022-12-04 17:24:12 +0000

    feat(4): 2nd algo

--- src/4/algo.ts
+++ src/4/algo.ts
@@ -8,7 +8,6 @@ const firstQuestion = () => {
 
     const isFirstInSecondRange = secondGroup[0]! >= firstGroup[0]! && secondGroup[1]! <= firstGroup[1]!
     const isSecondInFirstRange = firstGroup[0]! >= secondGroup[0]! && firstGroup[1]! <= secondGroup[1]!
-    // console.log(isFirstInSecondRange, isSecondInFirstRange)
     res += isFirstInSecondRange || isSecondInFirstRange ? 1 : 0
   }
   return res;
@@ -18,14 +17,12 @@ const firstQuestion = () => {
 
 const secondQuestion = () => {
   let res = 0;
-  for (let index = 0; index < 10; index += 2) {
+  for (let index = 0; index < inputData.length; index += 2) {
     const firstGroup = inputData[index]!
     const secondGroup = inputData[index + 1]!
 
-    // [60, 60], [45, 60],
-    const isFirstInSecondRange = secondGroup[0]! >= firstGroup[1]! && secondGroup[1]! >= firstGroup[0]!
-    const isSecondInFirstRange = firstGroup[0]! >= secondGroup[1]! && firstGroup[1]! >= secondGroup[0]!
-    console.log(isFirstInSecondRange, isSecondInFirstRange)
+    const isFirstInSecondRange = secondGroup[0]! <= firstGroup[1]! && secondGroup[1]! >= firstGroup[0]!
+    const isSecondInFirstRange = firstGroup[0]! <= secondGroup[1]! && firstGroup[1]! >= secondGroup[0]!
     res += isFirstInSecondRange || isSecondInFirstRange ? 1 : 0
   }
   return res;
