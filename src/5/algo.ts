260ae01a69233af0cb0a2423ae5e127d0d218f0c
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2022-12-05 13:13:36 +0000

    fix(4): extract moves parser

--- src/5/algo.ts
+++ src/5/algo.ts
@@ -3,20 +3,14 @@ import { inputData, naturalLanguageMovements } from './input.js';
 const firstQuestion = () => {
   const data = { ...inputData };
   naturalLanguageMovements.forEach((movement) => {
-    const getMoves = movement
-      .split(' ')
-      .filter((p) => Number.isInteger(Number.parseInt(p)))
-      .map((p) => Number.parseInt(p));
-
-    const pop = getMoves[0]!;
-    const fromColumn = getMoves[1]!.toString() as keyof typeof inputData;
-    const toColumn = getMoves[2]!.toString() as keyof typeof inputData;
-    const popItems = data[fromColumn]!.splice(-pop).reverse();
-    data[toColumn]!.push(...popItems);
+    const { pop, fromColumn: from, toColumn: to } = getMovements(movement);
+    const popItems = data[from]!.splice(-pop).reverse();
+    data[to]!.push(...popItems);
   });
-  const result = Object.keys(data).flatMap((p) =>
-    data[p as keyof typeof inputData].slice(-1),
-  ).join('');
+
+  const result = Object.keys(data)
+    .flatMap((p) => data[p as keyof typeof inputData].slice(-1))
+    .join('');
   const stacks = Object.keys(data).map((p) =>
     data[p as keyof typeof inputData].join(''),
   );
@@ -32,4 +26,17 @@ const secondQuestion = () => {
 export const Solution5 = {
   1: firstQuestion,
   2: secondQuestion,
+};
+
+const getMovements = (movement: string) => {
+  const getMoves = movement
+    .split(' ')
+    .filter((p) => Number.isInteger(Number.parseInt(p)))
+    .map((p) => Number.parseInt(p));
+
+  return {
+    pop: getMoves[0]!,
+    fromColumn: getMoves[1]!.toString() as keyof typeof inputData,
+    toColumn: getMoves[2]!.toString() as keyof typeof inputData,
+  };
 };
