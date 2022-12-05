0c589d04c1c95570e58cd8dbe072b25a2807e982
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2022-12-05 13:04:12 +0000

    perf(5): 1st algo

--- src/5/algo.ts
+++ src/5/algo.ts
@@ -1,13 +1,32 @@
-import { inputData } from './input.js';
+import { inputData, naturalLanguageMovements } from './input.js';
 
 const firstQuestion = () => {
-  return Number.NaN + inputData.length;
+  const data = { ...inputData };
+  naturalLanguageMovements.forEach((movement) => {
+    const getMoves = movement
+      .split(' ')
+      .filter((p) => Number.isInteger(Number.parseInt(p)))
+      .map((p) => Number.parseInt(p));
+
+    const pop = getMoves[0]!;
+    const fromColumn = getMoves[1]!.toString() as keyof typeof inputData;
+    const toColumn = getMoves[2]!.toString() as keyof typeof inputData;
+    const popItems = data[fromColumn]!.splice(-pop).reverse();
+    data[toColumn]!.push(...popItems);
+  });
+  const result = Object.keys(data).flatMap((p) =>
+    data[p as keyof typeof inputData].slice(-1),
+  ).join('');
+  const stacks = Object.keys(data).map((p) =>
+    data[p as keyof typeof inputData].join(''),
+  );
+  return { stacks_1_to_9: stacks, result };
 };
 
 /* -------------------------------------- */
 
 const secondQuestion = () => {
-  return Number.NaN + inputData.length;
+  return Number.NaN + naturalLanguageMovements.length;
 };
 
 export const Solution5 = {
