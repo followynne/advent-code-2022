a75c7491984d010723262caf3551e6afd770a2c7
matteoGregoricchio <32459930+followynne@users.noreply.github.com>
2022-12-06 08:00:25 +0000

    perf(6): both algos

--- src/6/algo.ts
+++ src/6/algo.ts
@@ -1,13 +1,45 @@
+import { onlyUnique } from '../util/filters.js';
 import { inputData } from './input.js';
 
 const firstQuestion = () => {
-  return Number.NaN + inputData.length;
+  const splitInput = inputData.split('');
+  let result = 0;
+  splitInput.forEach((_, i) => {
+    if (result !== 0 || splitInput.length - (3 + i) < 0) return;
+    const subset = [
+      splitInput[i]!,
+      splitInput[i + 1]!,
+      splitInput[i + 2]!,
+      splitInput[i + 3]!,
+    ];
+
+    if (subset.filter(onlyUnique).length === subset.length) {
+      result = i + 4;
+    }
+    return;
+  });
+  return result;
 };
 
 /* -------------------------------------- */
 
 const secondQuestion = () => {
-  return Number.NaN + inputData.length;
+  const splitInput = inputData.split('');
+  let result = 0;
+  splitInput.forEach((_, i) => {
+    if (result !== 0 || splitInput.length - (13 + i) < 0) return;
+    const subset = splitInput.filter(
+      (_, internalI) => internalI >= i && internalI < i + 14,
+    );
+    if (i === 0) {
+      console.log(subset);
+    }
+    if (subset.filter(onlyUnique).length === subset.length) {
+      result = i + 14;
+    }
+    return;
+  });
+  return result;
 };
 
 export const Solution6 = {
